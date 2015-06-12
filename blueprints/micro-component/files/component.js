f12ae063f5cdfda096ff55f0be01d4aaf0bdb01a
begedin <begedinnikola@gmail.com>
2015-06-12 15:27:26 +0200

    Cleaned up blueprint

--- blueprints/micro-component/files/component.js
+++ blueprints/micro-component/files/component.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classBindings: ['__name__']
+  classBindings: ['<%= componentName %>']
 });
--- blueprints/micro-component/index.js
+++ blueprints/micro-component/index.js
@@ -1,3 +1,13 @@
 module.exports = {
   description: 'The default blueprint for ember-cli micro-addon component.',
-}
+
+  locals: function(options) {
+    var componentEntity    = options.entity;
+    var componentName   = componentEntity.name;
+
+    return {
+      componentName: componentName,
+      emberCLIVersion: require('../../package').version,
+    };
+  },
+};
